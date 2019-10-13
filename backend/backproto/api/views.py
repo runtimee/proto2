c9aa19ef0942db099800fe43edc9e4e873f11fcd
Yan Guo <yanguo4@gmail.com>
2019-10-12 20:55:33 -0700

    Implemented echo (POST) and echo pong (GET)

--- backend/backproto/api/views.py
+++ backend/backproto/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CommonAPIView(views.APIView):
+    pass
+
+
+class EchoView(CommonAPIView):
+    permission_classes = []
+
+    def get(self, request, *args, **kwargs):
+        data_out = {'result': 'echo pong'}
+        return Response(
+            data_out,
+            status=status.HTTP_200_OK,
+        )
+
+    def post(self, request, *args, **kwargs):
+        return Response(
+            request.data,
+            status=status.HTTP_201_CREATED
+        )
