de484a196ee87aff1cdfd5af0354b235907e0e6c
Yan Guo <yanguo4@gmail.com>
2019-11-08 09:44:09 -0800

    Create project fairyland with Xcode 11.2

---
+++ ios13/fairyland/fairyland/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fairyland
+//
+//  Created by Yan Guo on 11/8/19.
+//  Copyright © 2019 Safaray Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
